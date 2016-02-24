a84bd56f37c19a05a5eec56334a005de3f37b51f
Alex <alexyan981@gmail.com>
2016-02-24 14:01:24 -0500

    Created the class for Ultrasonic Sensor

---
+++ USDevice.ino
@@ -0,0 +1,36 @@
+class UltrasonicSensor
+{
+	static constexpr double V_SOUND = 340.29 * 100.0 / 1000000.0;
+	int pin;
+
+public:
+	UltrasonicSensor(int pin = 7) :
+		pin(pin)
+	{}
+
+	inline double distance() const
+	{
+		pinMode(pin, OUTPUT);
+
+		digitalWrite(pin, LOW);
+		delayMicroseconds(2);
+
+		digitalWrite(pin, HIGH);
+		delayMicroseconds(5);
+
+		digitalWrite(pin, LOW);
+
+		pinMode(pin, INPUT);
+		long duration = pulseIn(pin, HIGH);
+		float distance = V_SOUND * duration;
+	}
+};
+
+UltrasonicSensor us;
+
+void setup() {}
+
+void loop()
+{
+	//
+}
