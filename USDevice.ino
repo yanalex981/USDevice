bc2f857ec78f1472769a9450a68cc925d4e603dc
Alex <alexyan981@gmail.com>
2016-02-26 18:16:16 -0500

    Added debug prints, and changed the implementation so that the device beeps rather than tones

--- USDevice.ino
+++ USDevice.ino
@@ -22,8 +22,8 @@ public:
 
     pinMode(pin, INPUT);
     long duration = pulseIn(pin, HIGH);
-    float distance = V_SOUND * duration;
-
+    double distance = V_SOUND * duration;
+    Serial.println(duration);
     return distance;
   }
 };
@@ -37,12 +37,42 @@ static constexpr double MAX_D = 400.0;
 static constexpr double MIN_FREQ = 500.0;
 static constexpr double MAX_FREQ = 6000.0;
 
-void setup() {}
+static constexpr int MIN_PAUSE = 200;
+static constexpr int MAX_PAUSE = 2000;
+
+static constexpr unsigned long BEEP_TIME = 50;
+
+static int last = 0;
+static int next = 0;
+#define DEBUGGING
+void setup()
+{
+#ifdef DEBUGGING
+  Serial.begin(9600);
+#endif
+}
 
 void loop()
 {
+  //tone(SPEAKER_PIN, 1024, BEEP_TIME);
   double distance = us.distance();
   double frequency = map(distance, MIN_D, MAX_D, MIN_FREQ, MAX_FREQ);
+  double pause = map(distance, MIN_D, MAX_D, MIN_PAUSE, MAX_PAUSE);
+  int t = millis();
 
-  tone(SPEAKER_PIN, frequency);
+  next = last + pause;
+
+  if (t >= next)
+  {
+    last = t;
+
+    //tone(SPEAKER_PIN, 1024, BEEP_TIME);
+  }
+
+#ifdef DEBUGGING
+  //Serial.print("L: ");
+  //Serial.print(last);
+  //Serial.print("D:");
+  //Serial.println(distance);
+#endif
 }
